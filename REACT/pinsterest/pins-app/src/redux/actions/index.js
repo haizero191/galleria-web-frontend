8e27d5c6694028abefd3b19ce6734a6bc4214180
Nguyễn Hải <44278262+haizero191@users.noreply.github.com>
2022-03-12 10:39:00 +0700

    Delete REACT/pinsterest/pins-app directory

--- REACT/pinsterest/pins-app/src/redux/actions/index.js
+++
@@ -1,6 +0,0 @@
-import {loadImages, findImages, showImage} from "./PictureActions";
-import {getUser} from "./UserAction";
-
-const combineAction = {loadImages, findImages, showImage, getUser}
-
-export default combineAction;
